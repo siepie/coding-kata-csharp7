930ca4c3ea9ef01082d9d82f8bbeadf811ad6df5
Simone <siepie@outlook.com>
2018-02-21 19:05:39 +0100

    Wrong project template

--- StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
@@ -1,10 +1,16 @@
-﻿using System;
+﻿using FluentAssertions;
 using Xunit;
 
 namespace StringCalculator.Tests
 {
     public class StringCalculatorTests
     {
-       
+        [Fact]
+        public void CalculateReturnsZeroAndZero()
+        {
+            string numbers = "";
+            (int sum, int difference) expected = (0, 0);
+            StringCalculator.Calculate(numbers).Should().Be(expected);
+        }
     }
 }

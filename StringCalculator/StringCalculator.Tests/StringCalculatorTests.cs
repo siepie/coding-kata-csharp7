ec37f680af9012d5721a73811670cf59ed3d8579
Simone <siepie@outlook.com>
2018-02-23 13:06:25 +0100

    Refactored unit test to show use of deconstruction of tuples

--- StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
+++ StringCalculator/StringCalculator.Tests/StringCalculatorTests.cs
@@ -9,40 +9,54 @@ namespace StringCalculator.Tests
         public void CalculateReturnsZeroAndZero()
         {
             string numbers = "";
-            (int sum, int difference) expected = (0, 0);
-            StringCalculator.Calculate(numbers).Should().Be(expected);
+            var (Sum, Difference) = StringCalculator.Calculate(numbers);
+            Sum.Should().Be(0);
+            Difference.Should().Be(0);
         }
 
         [Fact]
         public void CalculateWithOneNumberReturnsInputAsResultOnBoth()
         {
             string numbers = "1";
-            (int sum, int difference) expected = (1, 1);
-            StringCalculator.Calculate(numbers).Should().Be(expected);
+            var (Sum, Difference) = StringCalculator.Calculate(numbers);
+            Sum.Should().Be(1);
+            Difference.Should().Be(1);
         }
 
         [Fact]
         public void CalculateReturnsTwoPositiveNumbers()
         {
             string numbers = "9,6";
-            (int sum, int difference) expected = (15, 3);
-            StringCalculator.Calculate(numbers).Should().Be(expected);
+            var (Sum, Difference) = StringCalculator.Calculate(numbers);
+            Sum.Should().Be(15);
+            Difference.Should().Be(3);
         }
 
         [Fact]
         public void CalculateWithInputOfThreeNumbersReturnsTwoPositiveNumbers()
         {
             string numbers = "9,6,1";
-            (int sum, int difference) expected = (16, 2);
-            StringCalculator.Calculate(numbers).Should().Be(expected);
+            var (Sum, Difference) = StringCalculator.Calculate(numbers);
+            Sum.Should().Be(16);
+            Difference.Should().Be(2);
         }
 
         [Fact]
         public void CalculateWithInvalidInputUsesZeroAndReturnsPositiveNumbers()
         {
             string numbers = "9,zes,1";
-            (int sum, int difference) expected = (10, 8);
-            StringCalculator.Calculate(numbers).Should().Be(expected);
+            var (Sum, Difference) = StringCalculator.Calculate(numbers);
+            Sum.Should().Be(10);
+            Difference.Should().Be(8);
+        }
+
+        [Fact]
+        public void CalculateWithSeveralNumbers()
+        {
+            string numbers = "19,4,1,2,9";
+            var (Sum, Difference) = StringCalculator.Calculate(numbers);
+            Sum.Should().Be(35);
+            Difference.Should().Be(3);
         }
     }
 }

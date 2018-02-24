0ea55c0785304b3567998e417c412d625738bad5
Simone <siepie@outlook.com>
2018-02-24 16:03:01 +0100

    Added new kata

---
+++ FizzBuzz/FizzBuzz.Tests/FizzBuzzTests.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace FizzBuzz.Tests
+{
+    public class FizzBuzzTests
+    {
+        [Fact]
+        public void FizzBuzzReturnsWords()
+        {
+
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzz.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FizzBuzz.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("HP Inc.")]
+[assembly: AssemblyProduct("FizzBuzz.Tests")]
+[assembly: AssemblyCopyright("Copyright © HP Inc. 2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("b345ecd5-68d0-4552-ac2d-5a5a3724647b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]

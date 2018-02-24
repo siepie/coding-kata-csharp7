7f10ef6952eea2c9d566479e34b122324b6024ab
Simone <siepie@outlook.com>
2018-02-24 16:45:37 +0100

    Merge errors at start

--- FizzBuzz/FizzBuzz/FizzBuzz.cs
+++ FizzBuzz/FizzBuzz/FizzBuzz.cs
@@ -1,6 +1,13 @@
-﻿namespace FizzBuzz
+﻿using System;
+using System.Collections.Generic;
+
+namespace FizzBuzz
 {
     public class FizzBuzz
     {
+        public static List<string> FizzAndBuzz(List<object> input)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
